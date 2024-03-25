f7325bf0c9e3efa866036f6ed8bfc2e01dee64f9
Sniping_Duckling <raineeecarlooo23@gmail.com>
2024-03-25 16:45:55 +0800

    Update: Changed Fire on Board
    > Added a few scripts

---
+++ Assets/Scripts/FireScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireScript : MonoBehaviour
+{
+    // Add GameObject/Transform 
+    //Create method that detects if fire extinguisher collides destroy gameobject/setactive false
+    // wait for seconds load transition
+    //play audio when fire is inactive
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
