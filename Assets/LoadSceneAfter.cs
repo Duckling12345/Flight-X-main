f1ab161179669ad85a429e335b494314f91a88e7
Sniping_Duckling <raineeecarlooo23@gmail.com>
2024-03-31 21:45:20 +0800

    Update
    Update regarding video scenes/scripts

---
+++ Assets/LoadSceneAfter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class LoadSceneAfter : MonoBehaviour
+{
+
+    private VideoPlayer player;
+
+    public int buildIndex;
+
+    private void Awake() 
+    {
+        player = GetComponent<VideoPlayer>();
+    
+        player.Play();
+        player.loopPointReached += CheckOver; 
+      
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+}
